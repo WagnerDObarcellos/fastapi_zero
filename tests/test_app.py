bf3c8e79d6e68f6089fc54dac8a89f1f50763545
Wagner Diniz <wagner.diniz.barcellos@gmail.com>
2025-05-19 14:35:58 -0300

    Subindo o projeto do curso FastApi do zero

---
+++ tests/test_app.py
@@ -0,0 +1,21 @@
+from http import HTTPStatus
+from fastapi.testclient import TestClient
+from fastapi_zero.app import app
+
+def test_root_deve_retornar_ola_mundo():
+    """
+    Esse teste tem 3 etapas (AAA)
+    - A: Arrange - Arranjo
+    - A: Act     - Executa a coisa (o SUIT)
+    - A: Assert  - Garante que A é A.
+    """
+
+    # Arrange
+    client = TestClient(app)
+
+    # Act
+    response = client.get('/')
+
+    # Assert
+    assert response.status_code == HTTPStatus.OK # assert
+    assert response.json() == {'mensagem':'Olá, mundo'} # assert
